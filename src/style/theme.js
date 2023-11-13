8c89d7a975bc3335ca60887212aa50c8d2a8ae0d
silver <ekdldkaa@naver.com>
2023-11-13 23:35:27 +0900

    [feat]style시스템 설계

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,43 +1,58 @@
+import React from 'react';
+
 const theme = {
   colors: {
-    mainBackground: '#F9F1E7',
-    buttonColor: {
-      brown: '#B88E2F',
-      light: '#F9F1E7',
-      gray: '#F4F5F7 ',
-    },
-    fontColor: {
-      white: '#ffffff',
-      gray: '#9F9F9F',
-      black: '#000000',
-    },
+    /* 1이 제일 진한 색. b-파란색, x-초록색, g-회색*/
+    b1: '#002472',
+    b2: '#003CBC',
+    b3: '#2969F3',
+    b4: '#75A1FF',
+    x1: '#19592F',
+    x2: '#17993C',
+    x3: '#3BA54E',
+    x4: '#76C184',
+    g1: '#393939',
+    g2: '#5B5B5B',
+    g3: '#7D7D7D',
+    g4: '#979797',
+    g5: '#ADADAD',
+    g6: '#D0D0D0',
+    black: '#000000',
+    white: '#FFFFFF',
   },
-  fontStyles: {
-    title: `
-      font-family: "Poppins", sans-serif;
-      font-size: 4.8rem;
-      font-weight: 600;
-    `,
-    subTitle: `
-      font-family: "Poppins", sans-serif;
-      font-size: 3.6rem;
-      font-weight: 500;
-    `,
-    text: `
-      font-family: "Poppins", sans-serif;
-      font-size: 2.4rem;
-      font-weight: 500;
-    `,
-    subText: `
-      font-family: "Poppins", sans-serif;
-      font-size: 2rem;
-      font-weight: 400;
-    `,
-    logoText: `
-    font-family: "Yeseva One", sans-serif;
-    font-size: 2rem;
-    font-weight: 400;
-  `,
+  fonts: {
+    heading: `color: #000;
+    font-family: Pretendard;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 500;
+    text-transform: capitalize;`,
+    subText: `color: #646464;
+    font-family: Pretendard;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 600;
+    text-transform: capitalize;`,
+    body1: `color: #000;
+    font-family: Pretendard;
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 500;`,
+    body2: `color: #000;
+    font-family: Pretendard;
+    font-size: 0.75rem;
+    font-style: normal;
+    font-weight: 400;`,
+    caption1: `color: #BEBEBE;
+    font-family: Pretendard;
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 400;`,
+    caption2: `color: #000;
+    font-family: Pretendard;
+    font-size: 0.6875rem;
+    font-style: normal;
+    font-weight: 300;`,
   },
 };
 
