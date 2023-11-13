9ae1eb6b71c13ce10ab2fbdbdf373176ba4734d9
silver <ekdldkaa@naver.com>
2023-11-13 15:36:10 +0900

    [feat]스타일시스템예시파일

---
+++ src/style/theme.js
@@ -0,0 +1,44 @@
+const theme = {
+  colors: {
+    mainBackground: '#F9F1E7',
+    buttonColor: {
+      brown: '#B88E2F',
+      light: '#F9F1E7',
+      gray: '#F4F5F7 ',
+    },
+    fontColor: {
+      white: '#ffffff',
+      gray: '#9F9F9F',
+      black: '#000000',
+    },
+  },
+  fontStyles: {
+    title: `
+      font-family: "Poppins", sans-serif;
+      font-size: 4.8rem;
+      font-weight: 600;
+    `,
+    subTitle: `
+      font-family: "Poppins", sans-serif;
+      font-size: 3.6rem;
+      font-weight: 500;
+    `,
+    text: `
+      font-family: "Poppins", sans-serif;
+      font-size: 2.4rem;
+      font-weight: 500;
+    `,
+    subText: `
+      font-family: "Poppins", sans-serif;
+      font-size: 2rem;
+      font-weight: 400;
+    `,
+    logoText: `
+    font-family: "Yeseva One", sans-serif;
+    font-size: 2rem;
+    font-weight: 400;
+  `,
+  },
+};
+
+export default theme;
