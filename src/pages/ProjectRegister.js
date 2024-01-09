453ae7c50338fc6ff5bfd1e0b19f78ee28ab9b67
silver <ekdldkaa@naver.com>
2024-01-09 15:29:04 +0900

    [style]ProjectInputBody레이아웃 세팅

--- src/pages/ProjectRegister.js
+++ src/pages/ProjectRegister.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import StepHeader from '../components/register/StepHeader';
+import InputAgreementInfo from '../components/register/InputAgreementInfo';
+import InputExtraQuestionInfo from '../components/register/InputExtraQuestionInfo';
+import InputGetWayInfo from '../components/register/InputGetWayInfo';
+import InputGoodsInfo from '../components/register/InputGoodsInfo';
+import InputSellerInfo from '../components/register/InputSellerInfo';
 
 const ProjectRegister = () => {
+  const [step, setStep] = useState(1);
+
+  // 단계별 컴포넌트 배열
+  const InputInfoList = [
+    InputSellerInfo,
+    InputGetWayInfo,
+    InputGoodsInfo,
+    InputExtraQuestionInfo,
+    InputAgreementInfo,
+  ];
+  // step 값에 따른 컴포넌트 렌더링 함수
+  const renderStepBodyComponent = () => {
+    const StepBody = InputInfoList[step - 1];
+    return StepBody ? <StepBody /> : null;
+  };
+
   return (
     <Wrapper>
       <StepHeader step={1} />
-      <StepBody></StepBody>
+      <StepBodyWrapper>{renderStepBodyComponent()}</StepBodyWrapper>
     </Wrapper>
   );
 };
@@ -16,5 +37,12 @@ export default ProjectRegister;
 const Wrapper = styled.div`
   height: 100vh;
   padding-top: 10rem;
+
+  display: flex;
+  flex-direction: column;
 `;
-const StepBody = styled.div``;
+const StepBodyWrapper = styled.div`
+  height: 100%;
+  width: 85%;
+  margin-left: 3rem;
+`;
