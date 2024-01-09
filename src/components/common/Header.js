437c2f664708a441f73d09603b908cce94601b86
silver <ekdldkaa@naver.com>
2024-01-09 10:26:04 +0900

    [feat] 공통컴포 헤더 제작

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,29 +7,52 @@ import HeartIcon from '../../assets/icon/HeartIcon';
 
 const Header = () => {
   return (
-    <Wrapper>
-      <IconBox>
-        <HamburgerIcon />
-      </IconBox>
+    <HeaderWrapper>
+      <Wrapper>
+        <IconBox>
+          <HamburgerIcon />
+        </IconBox>
 
-      <WowLogo />
-      <IconBox>
-        <SearchIcon />
-      </IconBox>
-      <IconBox>
-        <HeartIcon />
-      </IconBox>
-    </Wrapper>
+        <CenteredLogo>
+          <WowLogo />
+        </CenteredLogo>
+
+        <IconBox>
+          <SearchIcon />
+          <HeartIcon />
+        </IconBox>
+      </Wrapper>
+    </HeaderWrapper>
   );
 };
+
+const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: white; /* Adjust as per your design */
+  z-index: 1000; /* Ensure it's above other content */
+`;
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
+  height: 20rem;
+  padding-left: 7rem;
+  padding-right: 7rem;
   border-bottom: 1px solid ${(props) => props.theme.colors.gray2};
 `;
-const IconBox = styled.div``;
+
+const IconBox = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+
+const CenteredLogo = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 export default Header;
