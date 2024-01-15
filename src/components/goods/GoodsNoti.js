81acbecdb6c78f781fc89803055a29ae79315a85
silver <ekdldkaa@naver.com>
2024-01-15 22:45:06 +0900

    [feat]문의글 입력창 추가

--- src/components/goods/GoodsNoti.js
+++ src/components/goods/GoodsNoti.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CommonButton from '../common/CommonButton';
 import NotiItem from './NotiItem';
 import styled from 'styled-components';
+import CommonInput from '../common/CommonInput';
+import CommonTextarea from '../common/CommonTextarea';
 
 const GoodsNoti = () => {
   const dummList = [
@@ -11,18 +13,30 @@ const GoodsNoti = () => {
     { title: '배송시작 공지', seller: '판매자', date: '2023. 09. 18' },
   ];
 
+  const [isInput, setIsInput] = useState(true);
+
   return (
-    <Wrapper>
-      <CommonButton
-        type={'lineBlue'}
-        children={'공지 작성하기'}
-        size={'l'}
-        width={'100%'}
-      />
-      {dummList.map((noti, index) => (
-        <NotiItem key={index} noti={noti} />
-      ))}
-    </Wrapper>
+    <>
+      {isInput ? (
+        <Wrapper>
+          <CommonInput size="l" placeholder="제목을 입력해주세요" />
+          <CommonTextarea size="l" placeholder="공지 내용을 입력해주세요" />
+          <CommonButton type={'fillBlue'} children={'등록하기'} size={'l'} />
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <CommonButton
+            type={'lineBlue'}
+            children={'공지 작성하기'}
+            size={'l'}
+            width={'100%'}
+          />
+          {dummList.map((noti, index) => (
+            <NotiItem key={index} noti={noti} />
+          ))}
+        </Wrapper>
+      )}
+    </>
   );
 };
 
